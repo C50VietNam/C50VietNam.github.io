700250288777eb61c565f89649043e67d1003861
root <lienminhthichsem@gmail.com>
2022-09-24 10:32:54 +0000

    test

---
+++ index.php
@@ -0,0 +1,70 @@
+<html xmlns="http://www.w3.org/1999/xhtml"><head>
+ <title>..:Hacked by Bui Tuan Duong..</title>
+<title>
+p.nen1 {
+        background-color: #000000;
+    }
+HACKED BY BUI TUAN DUONG
+</title>
+<script type="text/javascript" async="" src="https://www.google-analytics.com/analytics.js"></script><script>alert('Xin Chào');</script>
+<script>alert('Bạn Đã Bị Bắt');</script>
+
+    <title></title>
+    <link rel="Shortcut Icon" href="images/favicon.ico" type="image/x-icon">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta content="INDEX,FOLLOW" name="robots">
+</head>
+<body>
+ <title>C50 Việt Nam</title>
+<title>
+p.nen1 {
+        background-color: #000000;
+    }
+HACKED BY BUI TUAN DUONG
+</title>
+<script type="text/javascript" async="" src="https://www.google-analytics.com/analytics.js"></script><script></script>
+
+    <title></title>
+    <link rel="Shortcut Icon" href="images/favicon.ico" type="image/x-icon">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta content="INDEX,FOLLOW" name="robots">
+
+
+    <div style="text-align: center">
+    	
+        <br>
+    
+        <b></b>
+    </div>
+    <div style="text-align: center;">
+        <img src="https://img.upanh.tv/2022/09/24/FB33ECD6-3A6F-4ED6-9895-F082999B1B97.webp" alt="Logo">
+    </div>
+    <div style="text-align: center; font-size: 25px; padding-top: 20px; font-weight: bold;">
+        
+    </div>
+    <div style="text-align: center; font-size: 18px; padding-top: 5px;">
+        Hacker By TSOF2K6 &amp;&amp; Mr: Zero
+        <br>
+        
+    <div style="text-align: center; font-size: 15px; padding-top: 30px;">Trang Web Này Đã Bị Kiện Là Có Hành Vi Lừa Đảo Vì Thế Lượt Truy Cập Của Bạn Có Thể Bất Hợp Pháp Chúng Tôi Đã Lấy Được Ip Của Bạn Sau 1-5 Ngày Nữa Bạn Sẽ Phải Lên Công An Làm Việc
+    </div>
+    <div style="text-align: center; font-size: 15px; padding-top: 30px;">
+        Contact Me :
+        <br>
+        hoangcopyright.info@gmail.com
+    </div>
+
+<div style="text-align: center; font-size: 15px; padding-top: 5px;">
+<img alt="" src="http://www.wieistmeineip.de/ip-address">
+</div>
+ <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async="" src="https://www.googletagmanager.com/gtag/js?id=UA-129675600-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-129675600-1');
+</script>
+
+</body></html>
